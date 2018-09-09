c487c2ca06bd18088f090f326f4f63d22733b207
yixuanqi <b6t1b@ugrad.cs.ubc.ca>
2018-09-09 10:38:59 -0700

    Added ui package, main class, created main method and two other method (P1 Get Started!)

---
+++ src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Display_Hi();
+        Display_Welcome();
+        }
+
+    public static void Display_Hi(){
+        System.out.println("Hi");
+    }
+
+    public static void Display_Welcome(){
+        System.out.println("Welcome!");
+    }
+
+}
